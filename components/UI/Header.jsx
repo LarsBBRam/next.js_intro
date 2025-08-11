1805de385a37ce0f8ff6fa455e9bf0139915eea9
Lars Bendik Bjella Ramstad <brlb022025@gmail.com>
2025-08-11 13:34:54 +0200

    first final commit

--- components/UI/Header.jsx
+++ components/UI/Header.jsx
@@ -1,11 +1,24 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
 
 export const Header = () => {
+    const currentUrl = usePathname();
+
+    useEffect(() =>{
+      console.log(currentUrl);
+  
+    }, [currentUrl]);
     return(
           <header className="bg-slate-500 h-20 text-x1 flex justify-end items-center px-6 gap-6 text-white">
-            <Link href="/">Home</Link>
-            <Link href="/">SubPage</Link>
-            <Link href="/">DeepPage</Link>
+            {/* <button className = " text-orange-500 underline"> */}
+
+            <Link href="/" className = {`${currentUrl === "/" && "underline text-orange-500" } underline-offset-2`}>Home</Link>
+            {/* </button> */}
+            <Link href="/subPage" className = {`${currentUrl === "/" && "underline" } underline-offset-2`}>SubPage</Link>
+            <Link href="/deepPage" className = {`${currentUrl === "/" && "underline" } underline-offset-2`} >DeepPage</Link>
           </header>
 
     );
