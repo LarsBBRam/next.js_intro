b229282396b3cff10ccdfdead327f6ba5b78cbcc
Lars Bendik Bjella Ramstad <brlb022025@gmail.com>
2025-08-18 13:11:02 +0200

    Extra pages added, with simulated api call

---
+++ app/(pages)/animals/page.jsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+
+export default async function AnimalsPage() {
+  const fetchAnimals = await fetch("http://localhost:3000/api/animals", {
+    cache: "no-store",
+  });
+
+  const animals = await fetchAnimals.json();
+
+  console.log(animals);
+
+  return (
+    <>
+      <section className="min-h-screen py-24 flex flex-col justify-center items-center bg-purple-300">
+        <h1 className="bg-amber-300 ">Animals</h1>
+        <div className="grid grid-col-1 md:grid-cols-3 gap-6">
+          {animals.map((animal) => (
+            <div    className="p-4 bg-slate-800 text-white" 
+            key={animal.name + animal.id}>
+              <Image
+                src={animal.image}
+                alt={`picture of ${animal.name}, that is ${animal.description}`}
+                width={200}
+                height={200}
+                className="rounded-lg"
+              />
+              <h2 className="text-x1 font-semibold mt-4">{animal.name}</h2>
+              <p className="italic">{animal.species}</p>
+              <p className="text-sm mt-2">{animal.description}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+    </>
+  );
+}
---
+++ app/(pages)/magicProducts/page.jsx
@@ -0,0 +1,9 @@
+export default function MagicProductsPage(){
+    return (
+            <>
+      <section className=" min-h-screen flex justify-center items-center bg-blue-800">
+        <h1 className="text-7xl">Magic Products Page</h1>
+      </section>
+    </>
+    )
+}
